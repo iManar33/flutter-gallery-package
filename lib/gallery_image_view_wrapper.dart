d98680f5050008600d229202c29dfd74bd0106b5
iManar33 <imanarwhynot33@gmail.com>
2023-01-12 13:22:02 +0300

    using initialIndex property to specify the title of the initial index.

--- lib/gallery_image_view_wrapper.dart
+++ lib/gallery_image_view_wrapper.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
+
 // import 'package:url_launcher/link.dart';
 import 'gallery_item_model.dart';
 import 'galleryimage.dart';
@@ -36,13 +37,21 @@ class GalleryImageViewWrapper extends StatefulWidget {
 class _GalleryImageViewWrapperState extends State<GalleryImageViewWrapper> {
   final minScale = PhotoViewComputedScale.contained * 0.8;
   final maxScale = PhotoViewComputedScale.covered * 8;
-  List<String> titles=[''];
-  int _currentIndex = 3;
+  List<String> titles = [''];
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    _currentIndex = widget.initialIndex ?? 0;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text('${titles.last} (${_currentIndex+1} of ${widget.galleryItems.length})'),
+        title: Text(
+            '${widget.galleryItems[_currentIndex].title ?? ''} (${_currentIndex + 1} of ${widget.galleryItems.length})'),
         // Text('${_currentIndex+1} of ${widget.galleryItems.length}'),
         //
       ),
@@ -61,9 +70,6 @@ class _GalleryImageViewWrapperState extends State<GalleryImageViewWrapper> {
           scrollDirection: widget.scrollDirection,
           onPageChanged: (int index) {
             setState(() {
-
-             titles.add( widget.galleryItems[index].title?? '');
-             print(titles);
               _currentIndex = index;
             });
           },
@@ -76,30 +82,24 @@ class _GalleryImageViewWrapperState extends State<GalleryImageViewWrapper> {
   PhotoViewGalleryPageOptions _buildImage(BuildContext context, int index) {
     final GalleryItemModel item = widget.galleryItems[index];
     return PhotoViewGalleryPageOptions.customChild(
-
-    child:  Stack(
+      child: Stack(
           alignment: AlignmentDirectional.center,
           fit: StackFit.expand,
-        children : [
-          CachedNetworkImage(
-            imageUrl: item.imageUrl,
-            placeholder: (context, url) =>
-            const Center(child: CircularProgressIndicator()),
-            errorWidget: (context, url, error) => const Icon(Icons.error),
-          ),
-          if (item.isVideo == true)
-            link(galleryItem: item),
-      // Link(
-      //     uri: Uri.parse(item.videoUrl),
-      //     builder: (context, followLink) => OutlinedButton(
-      //       onPressed: followLink,
-      //       child:  const Image(image: AssetImage('images/youtube_icon.png', package: 'galleryimage')),
-      //     )),
-
-        ]),
-
-
-
+          children: [
+            CachedNetworkImage(
+              imageUrl: item.imageUrl,
+              placeholder: (context, url) =>
+                  const Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+            if (item.isVideo == true) link(galleryItem: item),
+            // Link(
+            //     uri: Uri.parse(item.videoUrl),
+            //     builder: (context, followLink) => OutlinedButton(
+            //       onPressed: followLink,
+            //       child:  const Image(image: AssetImage('images/youtube_icon.png', package: 'galleryimage')),
+            //     )),
+          ]),
       initialScale: PhotoViewComputedScale.contained,
       minScale: minScale,
       maxScale: maxScale,
